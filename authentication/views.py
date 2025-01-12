0325d3822a085a0078bc6337b17331b39de910a8
abusaidshabib <abusaidshabib712@gmail.com>
2025-01-12 20:57:12 +0600

    register complete

--- authentication/views.py
+++ authentication/views.py
@@ -1,8 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from authentication.forms import RegistrationForm
 
 # Create your views here.
 def login(request):
     return render(request, 'src/pages/login.html')
 
 def register(request):
-    return render(request, 'src/pages/signup.html')
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = RegistrationForm()
+
+    return render(request, 'src/pages/signup.html', {'form': form})
